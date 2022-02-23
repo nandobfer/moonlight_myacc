73dab75480e7bd58171cecd9fb566d847696ff8b
nandobfer <98974574+nandobfer@users.noreply.github.com>
2022-02-23 20:47:01 -0300

    Update config.local.php

--- config.local.php
+++ config.local.php
@@ -3,13 +3,14 @@
 $config['installed'] = true;
 $config['env'] = 'prod'; // dev or prod
 $config['mail_enabled'] = true;
-$config['server_path'] = '/home/nando_tibiabr/canary_server/';
+$config['server_path'] = 'C:/Games/canary_server/';
+// $config['server_path'] = '/root/canary_server/';
 $config['mail_admin'] = 'nandobfer@yahoo.com.br';
 $config['mail_address'] = 'nandobfer@yahoo.com.br';
 $config['date_timezone'] = 'America/Bahia';
 $config['client'] = '1260';
 $config['anonymous_usage_statistics'] = true;
-$config['session_prefix'] = 'myaac_zkp8nhff_';
-$config['cache_prefix'] = 'myaac_ukzv9hva_';
+$config['session_prefix'] = 'myaac_9u3dvr1m_';
+$config['cache_prefix'] = 'myaac_in7orvz9_';
 
 $config['highscores_ids_hidden'] = array(1, 2, 3, 4, 5);
